706664a104dc33f6562e44ffb5874016d304c2cd
javlocan <javlocan.dev@gmail.com>
2023-12-02 09:08:39 +0100

    idk anymore

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-echo "------------------------------------"
-echo "Let's start with the config setup!"
-echo "First, let's install the core packages that we need. 
-For some of them, your password is needed."
-
 SCRIPTS_DIR="$(pwd)/scripts"
 
 source "$SCRIPTS_DIR/00-export-variables.sh"
+
+echo $separator 
+echo "Let's start with the config setup!"
+echo "But first thing first. Let's update the system."
+echo $separator
+
+yay -Syu
+
+echo "Now, let's install the core packages that we need. 
+For some of them, your password is needed."
+
+
 source "$SCRIPTS_DIR/10-install-dotfiles.sh"
 source "$SCRIPTS_DIR/20-setup-dotfiles.sh"
 #source "$SCRIPTS_DIR/30-utils.sh"
